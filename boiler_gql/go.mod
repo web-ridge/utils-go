4c44af540bb2cc60b5b6c9aa8e9a6aa62162f2d9
Richard Lindhout <richardlindhout96@gmail.com>
2020-05-02 12:20:29 +0200

    Move helpers from generator to utils

---
+++ boiler_gql/go.mod
@@ -0,0 +1,11 @@
+module github.com/web-ridge/utils-go/boiler_gql
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.11.3
+	github.com/ericlagergren/decimal v0.0.0-20191206042408-88212e6cfca9
+	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334
+	github.com/volatiletech/null/v8 v8.1.0
+	github.com/volatiletech/sqlboiler/v4 v4.0.0
+)
---
+++ boiler_gql/input.go
@@ -0,0 +1,25 @@
+package boiler_gql
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+func GetInputFromContext(ctx context.Context, key string) map[string]interface{} {
+	requestContext := graphql.GetOperationContext(ctx)
+	variables := requestContext.Variables
+
+	// pick nested inputs
+	var ok bool
+	for _, splittedKey := range strings.Split(key, ".") {
+		variables, ok = variables[splittedKey].(map[string]interface{})
+		if !ok {
+			fmt.Println("can not get input from context for key: "+key+" splitted key: ", splittedKey)
+		}
+	}
+
+	return variables
+}
